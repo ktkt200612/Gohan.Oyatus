38144329def2e925e84d6a9873f6f4e50f5b5f30
ktkt200612 <ktkt200612@gmail.com>
2022-09-09 13:10:49 +0900

    message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+/**
      * Register any application services.
      *
      * @return void
-     */
-    public function register()
+     */public function register()
     {
-        //
+//
     }
 
-    /**
+/**
      * Bootstrap any application services.
      *
      * @return void
-     */
-    public function boot()
+     */public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }

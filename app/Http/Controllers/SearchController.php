49f45dc9d9fecdabb8c550dfe2a29c77d0232ac2
ktkt200612 <ktkt200612@gmail.com>
2022-09-09 18:11:21 +0900

    message

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,7 +17,7 @@ class SearchController extends Controller
 
     public function index() //検索ページ及び検索結果ページ表示
     {
-        $result = Store::orderBy("kana","asc")->get();
+        $result = Store::orderBy("kana","asc");
         return view('index', ['forms' => $result]);
     }
 
